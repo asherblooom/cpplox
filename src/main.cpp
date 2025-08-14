ac733c5ce26706a70dba321b55c2daae3002bdf1
Asher Bloom <si23870@bristol.ac.uk>
2025-08-14 14:06:08 +0100

    implement file reading and command line args

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,61 @@
-#include "chunk.hpp"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include "vm.hpp"
 
-int main(int argc, char *argv[]) {
-	Chunk chunk;
+static void repl();
+static void runFile(const std::string path);
+static std::string readFile(const std::string path);
 
-	chunk.WriteConstant(1.2, 2);
-	chunk.WriteConstant(3.4, 123);
-
-	chunk.Write(OP_ADD, 123);
-
-	chunk.WriteConstant(5.6, 123);
-
-	chunk.Write(OP_DIVIDE, 123);
-	chunk.Write(OP_NEGATE, 3);
-	chunk.Write(OP_RETURN, 3);
-
+int main(int argc, char* argv[]) {
 	VirtualMachine vm;
-	vm.Interpret(chunk);
 
+	if (argc == 1) {
+		repl();
+	} else if (argc == 2) {
+		runFile(argv[1]);
+	} else {
+		std::cerr << "Usage: clox [path]\n";
+		std::exit(65);
+	}
 	return 0;
+}
+
+static void repl() {
+	std::string line;
+	std::cout << "> ";
+	while (getline(std::cin, line)) {
+		// interpret(line);
+		std::cout << line << "\n";
+		std::cout << "> ";
+	}
+	std::cout << "\n";
+	return;
+}
+
+static void runFile(const std::string path) {
+	std::string source = readFile(path);
+	InterpretResult result;	 // = interpret(source);
+
+	if (result == INTERPRET_COMPILE_ERROR) exit(65);
+	if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
+static std::string readFile(const std::string path) {
+	std::ifstream file{path};
+	if (!file) {
+		std::cerr << "ERROR: Unable to open file: " << path << "\n";
+		exit(74);
+	}
+	std::string contents;
+	// move get position to end of file
+	file.seekg(0, std::ios::end);
+	// resize string with position of end of file (e.g. size of file)
+	contents.resize(file.tellg());
+	// reset to beginning
+	file.seekg(0, std::ios::beg);
+	file.read(&contents[0], contents.size());
+	file.close();
+
+	return contents;
 }
